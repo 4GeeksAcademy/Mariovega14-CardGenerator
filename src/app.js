1d54f63ef417fbfa1ad7c85b7895ef56e7da9c69
Mario Vega <marioalbe123@gmail.com>
2024-11-20 17:29:31 +0000

    Primer commit

--- src/app.js
+++ src/app.js
@@ -2,10 +2,35 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let numberCard = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+  let picas = ["♠", "♥", "♦", "♣"];
+
+  let card = document.querySelector("#root");
+  let topCard = document.createElement("div");
+  let numCard = document.createElement("div");
+  let bottomCard = document.createElement("div");
+
+  card.classList.add("card");
+  card.appendChild(topCard);
+  topCard.classList.add("topCard");
+  card.appendChild(numCard);
+  numCard.classList.add("numCard");
+  card.appendChild(bottomCard);
+  bottomCard.classList.add("bottomCard");
+
+  let randomindex = Math.floor(Math.random() * numberCard.length);
+  let randomPicas = Math.floor(Math.random() * picas.length);
+
+  topCard.textContent = picas[randomPicas];
+  numCard.textContent = numberCard[randomindex];
+  bottomCard.textContent = picas[randomPicas];
+
+  if (topCard.textContent === "♠" || topCard.textContent === "♣") {
+    topCard.classList.add("black");
+    bottomCard.classList.add("black");
+  } else if (topCard.textContent === "♥" || topCard.textContent === "♦") {
+    topCard.classList.add("red");
+    bottomCard.classList.add("red");
+  }
 };
